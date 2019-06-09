ed250f9682458907a487707e37e5c5e26316ec2c
joycenerd <joycenerd@csie.io>
2019-06-09 14:15:34 +0800

    change counter and didx

--- DD_FIR/DD_FIR.srcs/sources_1/imports/vivado_demo/code/sysctrl.v
+++ DD_FIR/DD_FIR.srcs/sources_1/imports/vivado_demo/code/sysctrl.v
@@ -7,8 +7,8 @@ module sysctrl(
 	output reg RW2,
 	output reg [5:0] didx1,
 	output reg [5:0] didx2,
-	output reg [3:0] cidx,
-	output reg [15:0] douta,
+	//output reg [3:0] cidx,
+	//output reg [15:0] douta,
 	output reg acc_en,
 	output reg bsy
 );
@@ -16,12 +16,63 @@ module sysctrl(
     reg CS, NS;
 	reg [3:0]counter;
 	reg cnt_en;
+
+	//next state logic
+	always@(*) begin
+		case(CS)
+			Idle: 
+				begin
+					if(ctrl[0])
+						NS=Calculate;
+					else
+						NS=Idle;
+				end
+			Calculate:
+				begin
+					if(didx2==Depth)
+						NS=Idle;
+					else
+						NS=Calculate;		
+				end
+			default: 
+				begin
+					NS=Idle;
+				end
+		endcase
+	end
+
+	//output logic
+	always@(*) begin
+		case(CS)
+			Idle: 
+				begin
+					RW2=&(~didx2);
+					RW1=1'b0;
+					acc_en=1'b0;
+					cnt_en=1'b0;
+					bsy=1'b0;
+				end
+			Calculate: 
+				begin	
+					RW2=(~counter[2])&(counter[1])&(counter[0]);
+					RW1=&(~counter);
+					acc_en=(counter[2])|(~counter[1])|(counter[0]);
+					cnt_en=1'b1;
+					bsy=1'b1;
+				end
+			default: 
+				begin
+					RW1=1'b0;
+					RW2=1'b0;
+					acc_en=1'b0;
+					cnt_en=1'b0;
+					bsy=1'b0;
+				end
+		endcase
+	end
 	
-	
-	 
-	 
     //counter
-    always @(posedge clk or negedge rst)begin
+    /*always @(posedge clk or negedge rst)begin
 	    if(~rst) begin
 		    counter<=4'd0;
 		end
@@ -31,12 +82,34 @@ module sysctrl(
 		else if(cnt_en)begin
 		    counter<=counter+1'b1;
 		end
+	end*/
+	always @(posedge clk ro negedge rst) begin
+		if(~rst) begin
+			counter<=4'd0;
+		end
+		else if(ctrl[1]) begin
+			counter<=4'd0;
+		end
+		else if(cnt_en) begin
+			counter<=counter+1'b1;
+		end
 	end
 	
 	//didx2
-	always @(posedge clk or negedge rst)begin
+	/*always @(posedge clk or negedge rst)begin
 	    if(~rst) begin
 		    didx2<= 6'd0;
+		end
+		else if(ctrl[1]) begin
+			didx2<= 6'd0;
+		end
+		else if(counter[2]&counter[1]&counter[0]) begin
+		    didx2<= didx2+1'b1;
+		end
+	end*/
+	always @(posedge clk or negedge rst) begin
+		if(~rst) begin
+			didx2 <= 6'd0;
 		end
 		else if(ctrl[1]) begin
 			didx2<= 6'd0;
